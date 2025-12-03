f4dd9639221a867ca27fadccb64542d194e53738
marcusmkl <marcoyachilles43@gmail.com>
2025-12-03 20:47:10 +0800

    chore: added tests for typescript organized files into folders deleted some files

---
+++ typescript tests/intro.ts
@@ -0,0 +1,11 @@
+// set properties for user variable
+let user = {name: "marcus", age: 20}
+
+// print
+console.log("mikel");
+console.log(user.name);  
+
+// types:
+// number, string, boolean, null, undefined, void, object
+// arrays, any, never, unknown
+
---
+++ typescript tests/variables.ts
@@ -0,0 +1,25 @@
+
+// syntax - let varName: type = value
+let greetings: string = "Hello Marcus";
+
+console.log(greetings);
+
+// variable then shows that ts catches error that toUpperCase method cant be used on a number
+let mynum = 6
+// mynum.toUpperCase()
+
+// numbers can be any integer or decimal 
+// this is redundunt since it has a type and a value of a number
+
+let userId: number = 334455.5
+
+// better code for number
+
+let adminId = 12345;
+adminId.toFixed();
+
+// boolean
+let isLoggedIn: boolean = false
+
+
+export {}
